79e3110cf727b1c7be07879b9a65e0a96624b4be
AndreyHein <soly.a@web.de>
2024-03-03 22:15:08 +0100

    homework21

---
+++ src/main/java/lesson21/homeWork21/RubberArray.java
@@ -0,0 +1,81 @@
+package lesson21.homeWork21;
+
+public class RubberArray {
+    private final int INIT_DATA_SIZE = 10;
+    private final float EXTEND_INDEX = 1.5f;
+    public int[] data;
+    private int length;
+
+    public RubberArray() {
+        data = new int[INIT_DATA_SIZE];
+        length = 0;
+    }
+
+    public int[] getDataArray() { return data; }
+
+    public int get(int idx) {
+        return data[idx];
+    }
+
+    public int[] extensionArray() {
+            // create new array
+            int[] newData = new int[(int) (data.length * EXTEND_INDEX)];
+            // move all items to new array
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+            // change link to new array
+            data = newData;
+        return newData;
+    }
+
+    public void add(int value) {
+        if (length == data.length) {
+            extensionArray();
+        }
+        // add value
+        data[length] = value;
+        length++;
+    }
+
+    public void add(int value, int idx) {
+        if (length == data.length) {
+            extensionArray();
+        }
+        for (int i = length; i >= idx; i--) {
+            data[i + 1] = data[i];
+        }
+        data[idx] = value;
+        length++;
+    }
+
+    public void remove(int idx) {
+        // move elements right to left from idx
+        for (int i = idx; i < data.length - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        length--;
+    }
+
+    public boolean contains(int value) {
+        return indexOf(value) != -1;
+    }
+
+    public int indexOf(int value) {
+        for (int i = 0; i < data.length; i++) {
+            if (value == data[i]) { return i; }
+        } return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("[");
+        for (int i = 0; i < length; i++) {
+            str.append(data[i]);
+            if (i < length - 1) {
+                str.append(", ");
+            }
+        }
+        return str.append("]").toString();
+    }
+}
---
+++ src/test/java/lesson21/homeWork21/RubberArrayTest.java
@@ -0,0 +1,61 @@
+package lesson21.homeWork21;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, hm # 21
+ * @author Andrey Hein
+ * @version 03-Mar-2024
+ */
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RubberArrayTest {
+    private int[] actual;
+//    private int length;
+    RubberArray array = new RubberArray();
+
+    @Test
+    public void testExtensionArray() {
+        actual = new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        Assertions.assertArrayEquals(actual, array.extensionArray());
+    }
+
+    @Test
+    public void testAdd() {
+        actual = new int[] {5,1,2,0,0,0,0,0,-3,0};
+        array.add(5);
+        array.add(2);
+        array.add(1,1);
+        array.add(-3,8);
+        Assertions.assertArrayEquals(actual,array.getDataArray());
+    }
+
+    @Test
+    public void testRemove() {
+        for (int i = 0; i < 10; i++) {
+            array.add(i);
+        }
+        actual = new int[] {0,1,2,3,4,5,6,7,8,9};
+        Assertions.assertArrayEquals(actual, array.getDataArray());
+        array.remove(2);
+        actual = new int[] {0,1,3,4,5,6,7,8,9,9};
+        Assertions.assertArrayEquals(actual, array.getDataArray());
+        array.remove(5);
+        actual = new int[] {0,1,3,4,5,7,8,9,9,9};
+        Assertions.assertArrayEquals(actual, array.getDataArray());
+    }
+
+    @Test
+    public void testIndexOf() {
+        for (int i = 0; i < 10; i++) {
+            array.add(i * 2);
+        }
+        actual = new int[] {0,2,4,6,8,10,12,14,16,18};
+        Assertions.assertArrayEquals(actual, array.getDataArray());
+        Assertions.assertEquals(7, array.indexOf(14));
+        Assertions.assertEquals(-1, array.indexOf(9));
+        // test method 'contains'
+        Assertions.assertTrue(array.contains(10));
+        Assertions.assertFalse(array.contains(17));
+    }
+}
